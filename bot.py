59267648ffef05987fc51ce06d43e1f477a9ff48
bondtegan12-eng <bondtegan12@gmail.com>
2026-07-16 13:44:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     from highrise.__main__ import main, BotDefinition
     
     # Force inject credentials directly into system environment variables
-    os.environ["api_token"] = "2c001cb06c4370e639be2d7a24cf4e7a0a860ef708d45d11cde0960633d0e8a6"
+    os.environ["api_token"] = token = "2c001cb06c4370e639be2d7a24cf4e7a0a860ef708d45d11cde0960653d0e8a6"
     os.environ["room_id"] = "64a094a74134ad0fd77b8734"
     
     loop = asyncio.get_event_loop()
